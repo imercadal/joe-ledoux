5fd5037f14de56887939cc88ef658d0bf5d47566
imercadal <ima280@nyu.edu>
2025-02-28 15:58:52 -0500

    cleaning up

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function NotFound() {
     return (
-        <main className='text-center'>
-            <h3 className='text-3xl'>There was a problem.</h3>
-            <p>We couldn&apost find the page you were looking for.</p>
-            <p>Got back to the <Link href='/'>Home Page</Link></p>
+        <main className='text-center p-12 gap-2'>
+            <h3 className='mb-5 text-3xl text-dark'>There was a problem.</h3>
+            <p>We couldn’t find the page you were looking for.</p>
+            <p>Got back to the <Link href='/' className="text-accent font-bold">Home Page</Link></p>
         </main>
     )
 }
