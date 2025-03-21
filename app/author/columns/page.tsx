02c1af5f4edfe724911e958bd05cb25b56b1b800
imercadal <ima280@nyu.edu>
2025-03-21 12:16:14 -0400

    New book covers

--- app/author/columns/page.tsx
+++ app/author/columns/page.tsx
@@ -4,7 +4,7 @@ import { posts } from './post-data';
 export default function ColumnPage(){
     return(
         <main className="bg-lightText">
-            <div className="relative mb-8 h-40 bg-cover bg-center bg-[url('/320_Author_Columns.webp')] flex items-center justify-center">
+            <div className="relative mb-8 h-40 bg-cover bg-center bg-[url('/350_Author_Columns.webp')] flex items-center justify-center">
                 <h3 className="font-bold">COLUMNS AND BLOGS</h3>
             </div>
             <PostList posts={ posts }/>
