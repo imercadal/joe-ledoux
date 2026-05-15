5b1fe74602e3a2c2e3d471cc87a28d10141e1a4d
imercadal <ima280@nyu.edu>
2026-05-15 12:11:32 -0400

    Redirect Four Realms printed URL

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/The-Four-Realms-of-Existence.html",
+        destination: "/author/67c773e439d91c4c35170cb4",
+        permanent: true,
+      },
+    ];
+  },
   /* config options here */
   /* In order to disable type check when building:
     Dangerously allow production builds to successfully complete even if
